a67bd8d0b042affe4972bc62d204f4761dcc6d8c
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2025-08-18 21:37:49 -0300

    finalizando section services e e dando inicio a section about

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,3 +38,18 @@ ScrollReveal().reveal(".service__card", {
     ...scrollRevealOption,
     interval: 500,
 });
+
+ScrollReveal().reveal(".service__btn", {
+    ...scrollRevealOption,
+    interval: 2000,
+});
+
+ScrollReveal().reveal(".about__container .section__header", {
+    ...scrollRevealOption,    
+});
+
+ScrollReveal().reveal(".about__list li", {
+    ...scrollRevealOption,
+    delay: 500,
+    interval: 500,
+});
